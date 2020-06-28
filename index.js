3f11c0d5c7f10869fffa464e39cebf1c46c3ade7
码农小兴 <jiaxing_zheng@hotmail.com>
2020-06-28 19:07:04 +0800

    add package.json

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ const inflate = require('inflation');
 
 log4js.configure({
   appenders: {
-    everything: { type: 'file', filename: './logs/all.log' }
+	'file': { type: 'file', filename: './logs/all.log' },
+	'out': { type: 'stdout' }
   },
   categories: {
-    default: { appenders: [ 'everything' ], level: 'debug' }
+    default: { appenders: [ 'file', 'out' ], level: 'debug' }
   }
 });
 
 const logger = log4js.getLogger();
+
+function logClientInfo(req) {
+	const ip = req.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
+	req.connection.remoteAddress || // 判断 connection 的远程 IP
+	req.socket.remoteAddress || // 判断后端的 socket 的 IP
+	req.connection.socket.remoteAddress;
+
+	logger.info("client ip: ", ip);
+	logger.info("client port: ", req.socket.remotePort);
+}
 
 http.createServer((req, res) => {
 	const opts = {};
@@ -23,10 +34,12 @@ http.createServer((req, res) => {
 	opts.limit = opts.limit || '1mb';
 	const strict = opts.strict !== false;
 	raw(inflate(req), opts).then(str => {
+		logger.info(req);
 		logger.info('-----------------------------------------------------');
-		logger.info([req.method, req.url, 'HTTP/', req.httpVersion].join(' '));
+		logger.info([req.method, req.url, 'HTTP/' + req.httpVersion].join(' '));
 		logger.info(req.rawHeaders);
-		logger.info(str);
+		if (str != '') logger.info(str);
+		logClientInfo(req);
 	});
   	res.writeHead(200, { 'Content-Type': 'text/plain' });
   	res.end('okay');
